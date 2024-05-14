16a63b9b896e8f68a3f9daf4637fe0f9c81ffc75
Nicolas Simoncini <nsimoncini@netsergroup.us>
2024-05-14 11:06:08 -0300

    Handling publications consumer plugin

---
+++ src/Backend/Plugins/TFEHelper.Backend.Plugins.PluginBase/Interfaces/IPublicationsConsumerPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFEHelper.Backend.Plugins.PluginBase.Specifications.PublicationsCollector.Classes;
+
+namespace TFEHelper.Backend.Plugins.PluginBase.Interfaces
+{
+    internal interface IPublicationsConsumerPlugin : IBasePlugin
+    {
+        Task<bool> SendPublicationsAsync(IEnumerable<PublicationPLG> publications, CancellationToken cancellationToken = default);
+    }
+}
